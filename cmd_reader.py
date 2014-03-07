bbcb8bb77b5bdfc0e8889c40e11f597858f887f1
Saoud Salah Eddine <saoud92@gmail.com>
2014-03-07 22:08:23 +0100

    Initial commit.

---
+++ cmd_reader.py
@@ -0,0 +1,10 @@
+# Cmd_reader : command reader is program that read an interpert the option
+# entered with the command and their paramters.
+
+# to do:
+#   Implement a funtion that reads individual entiers, i.e. -p -s -j -k
+#   Implement a function that reads individual entiers with their parameters,
+#       i.e. -p /path/ -s source
+#   The previous function with add support to grouped paramters -psj , and with
+#   The possibility of the later one accepts an argument.
+#   Long parameters support.
